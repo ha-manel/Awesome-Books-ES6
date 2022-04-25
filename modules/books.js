254ac50bb2dbe000ac849dee6fa34ff001c81d0b
Manel <manelhammouche3@gmail.com>
2022-04-25 10:26:27 +0100

    Add books class module

---
+++ modules/books.js
@@ -0,0 +1,33 @@
+export default class BookCollection {
+  constructor() {
+    this.booksArray = JSON.parse(localStorage.getItem('books')) || [];
+  }
+
+  addBook = (title, author) => {
+    const book = {
+      title,
+      author,
+    };
+    this.booksArray.push(book);
+    localStorage.setItem('books', JSON.stringify(this.booksArray));
+    this.displayBooks();
+  }
+
+  removeBook = (index) => {
+    this.booksArray.splice(index, 1);
+    localStorage.setItem('books', JSON.stringify(this.booksArray));
+    this.displayBooks();
+  }
+
+  displayBooks = () => {
+    const booksDiv = document.querySelector('.books');
+    booksDiv.innerHTML = '';
+    this.booksArray.forEach((book, id) => {
+      booksDiv.innerHTML += `<div class="book-container"><div class="book-info"><p class="display-title">" ${book.title.charAt(0).toUpperCase() + book.title.slice(1)} "</p><p>by</p><p class="display-author">${book.author.charAt(0).toUpperCase() + book.author.slice(1)}</p></div><button id="${id}" class="remove-btn">Remove</button></div>`;
+      const removeBtn = document.getElementById(`${id}`);
+      removeBtn.addEventListener('click', () => {
+        this.removeBook(id);
+      });
+    });
+  }
+}
