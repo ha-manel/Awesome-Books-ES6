83c178ef5fc9e1f9e0578e5cd20e62597bc941f6
Manel <manelhammouche3@gmail.com>
2022-05-14 23:42:57 +0100

    Create saveAndRender method

--- modules/books.js
+++ modules/books.js
@@ -3,27 +3,30 @@ export default class BookCollection {
     this.booksArray = JSON.parse(localStorage.getItem('books')) || [];
   }
 
+  saveAndRender = () => {
+    localStorage.setItem('books', JSON.stringify(this.booksArray));
+    this.displayBooks();
+  }
+
   addBook = (title, author) => {
     const book = {
       title,
       author,
     };
     this.booksArray.push(book);
-    localStorage.setItem('books', JSON.stringify(this.booksArray));
-    this.displayBooks();
+    this.saveAndRender();
   }
 
   removeBook = (index) => {
     this.booksArray.splice(index, 1);
-    localStorage.setItem('books', JSON.stringify(this.booksArray));
-    this.displayBooks();
+    this.saveAndRender();
   }
 
   displayBooks = () => {
     const booksDiv = document.querySelector('.books');
     booksDiv.innerHTML = '';
     this.booksArray.forEach((book) => {
-      booksDiv.innerHTML += `<div class="book-container"><div class="book-info"><p class="display-title">" ${book.title.charAt(0).toUpperCase() + book.title.slice(1)} "</p><p>by</p><p class="display-author">${book.author.charAt(0).toUpperCase() + book.author.slice(1)}</p></div><button class="remove-btn">Remove</button></div>`;
+      booksDiv.innerHTML += `<div class="book-container"><div class="book-info"><p class="display-title">" ${book.title} "</p><p>by</p><p class="display-author">${book.author}</p></div><button class="remove-btn">Remove</button></div>`;
     });
     const removeBtn = document.querySelectorAll('.remove-btn');
     removeBtn.forEach((btn, index) => {
